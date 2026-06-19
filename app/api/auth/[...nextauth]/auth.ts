46fa3f24dc2620d6b277e89f484d29428cf4451b
rafaelralvesjj <rafaelralvesjj@gmail.com>
2026-06-19 10:26:40 -0300

    Update auth.ts

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -54,8 +54,8 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.id = user.id;
-        token.role = user.role;
+      token.id = user.id;
+      token.role = user.role ?? "";
       }
       return token;
     },
