02b29b6df8f0c6e6e383adbcb69264eb92f66ba2
rafaelralvesjj <rafaelralvesjj@gmail.com>
2026-06-22 10:41:00 -0300

    Update route.ts

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
         id: true,
         name: true,
         email: true,
-        imageUrl: true,
+        image: true,
       },
     });
     return NextResponse.json(students);
