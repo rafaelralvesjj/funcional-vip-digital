f0f9749988288cf694aaf5d3867ca5b0ef00a384
rafaelralvesjj <rafaelralvesjj@gmail.com>
2026-07-27 18:33:37 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  experimental: {
-    serverComponentsExternalPackages: ["@vercel/blob", "undici"],
-  },
-
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        undici: path.resolve(__dirname, "lib/undici-browser.js"),
-      };
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
